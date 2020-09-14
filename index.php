68d793459defa36aa3151884a26ee7949c49ede2
refrain <re.frain17418@gmail.com>
2020-09-14 23:58:22 +0700

    sweetalert library testing

--- index.php
+++ index.php
@@ -5,20 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrasi | Taekwondo IDTF </title>
     <link rel="stylesheet" href="style.css">
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 </head>
 <body>
-
-<ul>
-    <li> <a href="#">Biodata</a></li>
-    <li><a href="#">Pembayaran</a></li>
-    <li><a href="#">Teknik</a></li>
-    <li><a href="#">Ujian</a></li>
+    
+    <ul>
+        <li> <a href="#">Biodata</a></li>
+        <li><a href="#">Pembayaran</a></li>
+        <li><a href="#">Teknik</a></li>
+        <li><a href="#">Ujian</a></li>
     <li><a href="#">Gasuku</a></li>
     <li><a href="#">Pertandingan</a></li>
     <li><a href="#">Jadwal Latihan</a></li>
     <li><a href="#">Tempat Latihan</a></li>
 </ul>
-    
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+<script>
+    document.window.eventHandler("onload", function () {
+        swal("Hello world!");
+    })
+</script>
 </body>
 </html>
